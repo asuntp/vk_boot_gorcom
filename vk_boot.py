8ef0ff1c425e22ae2007ecee0b9c916a543e2cb3
asuntp <asuntp81@gmail.com>
2020-08-12 19:58:15 +0300

    update

---
+++ vk_boot.py
@@ -0,0 +1,26 @@
+import vk_api
+from random import randint
+from config import *
+
+#создаем сессию
+
+vk = vk_api.VkApi(token=token)
+#авторизуем токен
+vk._auth_token()
+
+#проверка на сервере вк приходящие сообщения
+try:
+    while True:
+        messages = vk.method("messages.getConversations", {"offset": 0, "count": 20, "filter": "unanswered "})
+        if messages["count"]>=1:
+            text = messages["items"][0]["last_message"]["text"]
+            user_id = messages["items"][0]["last_message"]["from_id"]
+            if text.lower() = "привет":
+                vk.method("message.send", {"user_id": user_id, "message": "Привет",
+                                           "random_id": randint(1, 1000)})
+            else:
+                vk.method("message.send", {"user_id": user_id, "message": "Привет, я бот, ничего не понимаю",
+                                       "random_id": randint(1, 1000)})
+
+
+
